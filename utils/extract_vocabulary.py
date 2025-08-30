9714df32fc4112e039c7018ba9a9b42c2a0a5ec1
jonahschlie <jonah.schlie@gmail.com>
2025-08-30 16:07:27 +0200

    Remove model_*k.json artifacts

--- utils/extract_vocabulary.py
+++
@@ -1,6 +0,0 @@
-def extract_vocabulary(text: str):
-    # use to get our starting set based on corpus
-    vocabulary = set()  # using a set automatically filters duplicates
-    for letter in text.lower():
-        vocabulary.add(letter)
-    return vocabulary
